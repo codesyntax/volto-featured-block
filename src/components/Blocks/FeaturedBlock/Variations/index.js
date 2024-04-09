50b7f34ee99eccd7939b3c1db4cf4020e04db7d0
Mikel Larreategi <mlarreategi@codesyntax.com>
2024-04-09 21:07:49 +0200

    lint

--- src/components/Blocks/FeaturedBlock/Variations/index.js
+++ src/components/Blocks/FeaturedBlock/Variations/index.js
@@ -3,4 +3,3 @@ import CarouselFeaturedView from './CarouselFeaturedView';
 export { FeaturedViewImageLeft } from './FeaturedViewImageLeft';
 export { FeaturedViewImageRight } from './FeaturedViewImageRight';
 export { CarouselFeaturedView };
-
