08afc7d35a31107b01735355f9d9dbe5f27ba4b1
Mikel Larreategi <mlarreategi@codesyntax.com>
2024-04-09 18:39:53 +0200

    use block extensions

--- src/components/Blocks/FeaturedBlock/FeaturedBlockView.jsx
+++ src/components/Blocks/FeaturedBlock/FeaturedBlockView.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import config from '@plone/volto/registry';
+import cx from 'classnames';
+import { withBlockExtensions } from '@plone/volto/helpers';
 
 const FeaturedBlockView = (props) => {
-  const { data } = props;
+  const { data, className = '' } = props;
   const variationsConfig = config.blocks.blocksConfig['csFeatured'].variations;
   let BodyTemplate = '';
   for (let variation in variationsConfig) {
@@ -11,6 +13,9 @@ const FeaturedBlockView = (props) => {
     }
   }
 
-  return <BodyTemplate data={data} isEditMode={false} {...props} />;
+  return <div className={cx('block featured-block', className)}>
+    <BodyTemplate data={data} isEditMode={false} {...props} />;
+  </div>
 };
-export default FeaturedBlockView;
+export default withBlockExtensions(FeaturedBlockView);
+ 
