25be3f974b1d8ed6110d6544d2140af5c51af84b
codesyntax <codesyntax@topinburu.local>
2024-12-24 13:03:06 +0100

    remove barrel import files

--- packages/volto-featured-block/src/components/Blocks/FeaturedBlock/Variations/index.js
+++
@@ -1,5 +0,0 @@
-export { CarouselFeaturedView } from './CarouselFeaturedView';
-export { FeaturedViewImageLeft } from './FeaturedViewImageLeft';
-export { FeaturedViewImageRight } from './FeaturedViewImageRight';
-export { FeaturedViewImageLeftRichText } from './FeaturedViewImageLeftRichText';
-export { FeaturedViewImageRightRichText } from './FeaturedViewImageRightRichText';
--- packages/volto-featured-block/src/components/Blocks/FeaturedBlock/index.js
+++
@@ -1,4 +0,0 @@
-import FeaturedBlockView from './FeaturedBlockView';
-import FeaturedBlockEdit from './FeaturedBlockEdit';
-
-export { FeaturedBlockView, FeaturedBlockEdit };
